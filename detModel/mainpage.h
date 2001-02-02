6892e769514857d9c7c3c71d1c01c6d72b048627
jrb <>
2001-02-02 00:26:23 +0000

    Import of package for generic geometry model

---
+++ detModel/mainpage.h
@@ -0,0 +1,20 @@
+// Mainpage for doxygen
+
+/*! \mainpage package detModel
+
+ This package provides an interface to a generic detector description
+ or model suitable for use by an application such as Simulation 
+ or Reconstruction.  
+ <p>Currently, the source of the model is an xml file, such as flight.xml,
+ which describes a particular detector according to the document type
+ description in ggd.dtd, but clients of <strong>detModel</strong>
+ are insulated from  the xml source.  These files may be found in the xml 
+ subdirectory of <strong>detModel</strong></p>
+ 
+\section reference Reference Document
+
+  <hr>
+  \section requirements requirements
+  \include requirements
+
+*/
