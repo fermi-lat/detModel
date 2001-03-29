81f5fab153f7372c5005dc41e3ceb0f4d362bc47
riccardo <>
2001-03-29 14:59:23 +0000

    Added GDDmatrix

---
+++ src/GDDmatrix.cpp
@@ -0,0 +1,45 @@
+#include "detModel/GDDmatrix.h"
+
+void GDDtranslate(GDDvector* t, GDDvector* v)
+{
+  v->x = v->x + t->x;
+  v->y = v->y + t->y;
+  v->z = v->z + t->z;
+};
+
+/// 0 = x, 1 = y, 2 = z
+void GDDrotate(int dir, double theta, GDDvector* v)
+{
+  GDDvector* temp = new GDDvector(v->x, v->y, v->z);
+
+  theta = theta*GDDPI/180;
+
+  switch(dir){
+  case 0:
+    {
+      v->x = temp->x;
+      v->y = temp->y*cos(theta) - temp->z*sin(theta);
+      v->z = temp->y*sin(theta) + temp->z*cos(theta);
+    }
+    break;
+
+  case 1:
+    {
+      v->x = temp->x*cos(theta) + temp->z*sin(theta);
+      v->y = temp->y;
+      v->z = temp->z*cos(theta) - temp->x*sin(theta);
+    }
+    break;
+
+  case 2:
+    {
+      v->x = temp->x*cos(theta) - temp->y*sin(theta);
+      v->y = temp->x*sin(theta) + temp->y*cos(theta);
+      v->z = temp->z;
+    }
+    break;
+  }
+
+  delete temp;
+
+}
---
+++ src/Sections/GDDbox.cpp
@@ -0,0 +1,17 @@
+#include "detModel/GDDpurge.h"
+
+#include "detModel/Management/GDDsectionsVisitor.h"
+#include "detModel/Sections/GDDbox.h"
+
+
+void GDDbox::Accept(GDDsectionsVisitor* v){
+  v->visitBox(this);
+  if (seg)v->visitSeg(seg);
+}
+
+
+void GDDbox::constructBB(){
+  setBBX(getX());
+  setBBY(getY());
+  setBBZ(getZ());
+}
