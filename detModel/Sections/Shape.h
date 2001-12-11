f41454b6a916068d39358d4646b2e6cac828ba5b
riccardo <>
2001-12-11 10:25:39 +0000

    Added support for new detectorType attribute in gdd.dtd

--- detModel/Sections/Shape.h
+++ detModel/Sections/Shape.h
@@ -16,32 +16,39 @@ namespace detModel{
     /** Some constructors; since this is an abstract class, these
      * constructors are useful only in its concrete subclasses
      */
-    Shape(std::string pname):Volume(pname),sensitive(0){units = new Units;}
-    Shape():Volume(),sensitive(0){units = new Units;}
+    Shape(std::string pname):Volume(pname),m_detectorType(0),m_sensitive(0){m_units = new Units;}
+    Shape():Volume(),m_detectorType(0),m_sensitive(0){m_units = new Units;}
 
-    virtual ~Shape(){delete units;};
+    virtual ~Shape(){delete m_units;};
 
     /**
-     * With this method it is possible to set the shape
+     * With this method it is possible to set and get the shape
      * sensitive for simulations purpouses
      */
-    void setSensitive(bool psensitive){sensitive = psensitive;};
-    bool getSensitive(){return sensitive;};
+    void setSensitive(bool psensitive){m_sensitive = psensitive;};
+    bool getSensitive(){return m_sensitive;};
+    /**
+     * With this method it is possible to set and get the 
+     * detector type attribute (see the gdd.dtd for meaning)
+     */
+    void setDetectorType(bool pdt){m_detectorType = pdt;};
+    bool getDetectorType(){return m_detectorType;};
     /**
      * This method set the material (a string) of the shape
      */  
-    void setMaterial(std::string pmaterial){material = pmaterial;};
+    void setMaterial(std::string pmaterial){m_material = pmaterial;};
     /**
      * This method gives back the material (a string) of the shape
      */  
-    std::string getMaterial(){return material;};
+    std::string getMaterial(){return m_material;};
 
-    Units* getUnits(){return units;};
+    Units* getUnits()const{return m_units;};
 
   private:
-    std::string material;
-    bool sensitive;          
-    Units* units;
+    unsigned int m_detectorType;
+    std::string m_material;
+    bool m_sensitive;          
+    Units* m_units;
   };
 }
 #endif //SHAPE_H
