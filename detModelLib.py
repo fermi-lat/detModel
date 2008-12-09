2380dc3ec3a3646cecd1f60869af4f0539898850
ecephas <>
2008-12-09 00:08:45 +0000

    Check in detModelLib.py for detModel v2r21 (branch-tag, GlastReleasev15r49).

--- detModelLib.py
+++ detModelLib.py
@@ -1,7 +1,8 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/detModel/detModelLib.py,v 1.1 2008/07/09 21:13:46 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['detModel'])
-
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('xmlUtilLib')
 def exists(env):
     return 1;
