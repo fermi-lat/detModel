e2cecb1d1878a832f02b14b18b6b98126b73294b
jrb <>
2009-08-06 23:59:02 +0000

    SConscript: use registerTargets; eliminate lines belonging in detModelLib.py detModelLib.py:  explicitly list all packages directly used by detModel

--- detModelLib.py
+++ detModelLib.py
@@ -1,8 +1,11 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/detModel/detModelLib.py,v 1.1 2008/07/09 21:13:46 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/detModel/detModelLib.py,v 1.3 2009/01/23 00:21:07 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['detModel'])
     env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('xmlUtilLib')
+    env.Tool('xmlBaseLib')
+    env.Tool('identsLib')
+    env.Tool('addLibrary', library = env['xercesLibs'])
 def exists(env):
     return 1;
