17715cf824c317c5019341b32bc2cfb7f2d48a3b
jrb <>
2012-08-16 23:57:27 +0000

    for Windows no-installed-headers

--- detModelLib.py
+++ detModelLib.py
@@ -1,11 +1,21 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/detModel/detModelLib.py,v 1.3 2009/01/23 00:21:07 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/detModel/detModelLib.py,v 1.4 2009/08/06 23:59:02 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['detModel'])
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'detModel') 
+            env.Tool('findPkgPath', package = 'xmlBase') 
+            env.Tool('findPkgPath', package = 'facilities') 
+
     env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('xmlUtilLib')
     env.Tool('xmlBaseLib')
     env.Tool('identsLib')
     env.Tool('addLibrary', library = env['xercesLibs'])
+
+    if kw.get('incsOnly', 0) == 1: 
+        env.Tool('findPkgPath', package = 'facilities')        
+        env.Tool('findPkgPath', package = 'xmlBase')        
+        env.Tool('findPkgPath', package = 'xmlUtil')        
 def exists(env):
     return 1;
